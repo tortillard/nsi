5638889cc4c5b0346f0b904fafa2d4b2e098aabc
tortillard <theotortillard@gmail.com>
2025-03-26 22:10:13 +0100

    add

---
+++ premiere/langages_et_programmation/fonctions_classiques_exo.py
@@ -0,0 +1,72 @@
+#1. Ecrire une fonction est_present(element, li)
+# qui renvoie False si element n'est pas dans li, et qui
+# renvoie True si le element est dans li.  
+
+def est_present(element, li):
+    pass
+
+# 2. Ecrire une fonction recherche_indice_fin(element, li)
+# qui renvoie -1 si nombre n'est pas dans li, et qui renvoie
+# l'indice de element dans la li, la dernière fois
+# où on le rencontre
+
+def recherche_indice_fin(element, li):
+    pass
+
+#3. Ecrire une fonction recherche_indice_debut(element, li)
+# qui renvoie -1 si nombre n'est pas dans li, et qui renvoie
+# l'indice de nombre dans li, la première fois
+# où on le rencontre
+
+def recherche_indice_debut(element, li):
+    pass
+
+#4. Ecrire une fonction occurence(element, li)
+# qui renvoie 0 si element n'est pas dans li, et qui sinon
+# renvoie l'occurence de element dans li.
+# (le nombre de fois ou nombre est dans table)
+
+def occurence(element, li):  
+    pass
+
+#5.Ecrire une fonction somme(li) qui calcule la somme
+# des nombres de li.
+def somme(li):
+    pass
+
+#6.Ecrire une fonction moyenne(li) qui calcule la moyenne
+# des éléments de li.  
+def moyenne(li):    
+    pass
+
+#7. Ecrire une fonction minimum(li) qui renvoie
+# la liste composé du nombre minimum de li et l'indice de ce minimum 
+
+def minimum(li):
+    pass
+
+#8. Ecrire une fonction maximum(li) qui renvoie 
+# la liste composé du nombre maximum de li et l'indice de ce maximum
+  
+def maximum(li):
+    pass
+
+#9. Ecrire une fonction tout_est_pair(li) qui renvoie 
+# True si tout les nombres contenu dans li sont pairs et False sinon. 
+# On renvoi True si aucun nombre est présent dans la liste 
+
+def tout_est_pair(li):
+    pass 
+
+
+
+# 10. Fonction factorielle : 
+#   n! = n*(n-1)*(n-2) *.....*3*2*1
+#   5! = 5*4*3*2*1 = 120
+#   Par convention 0! = 1
+# Ecrire une fonction factorielle(n) qui renvoie la factorielle de n
+
+def factorielle(n):
+    pass
+
+
